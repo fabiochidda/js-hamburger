74b50b7214d28dce4b157f909a491897590a8029
fabiochidda <fabio.chidda99@gmail.com>
2022-02-09 02:52:27 +0100

    Dichiarate le costanti per il menu e per l'icona del menu

---
+++ main.js
@@ -0,0 +1,7 @@
+const navHamburgerButton = document.querySelector(".fa-bars");
+console.log(navHamburgerButton);
+
+const navHamburgerMenu = document.querySelector(".hamburger-menu");
+console.log(navHamburgerMenu);
+
+
